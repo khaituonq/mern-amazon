6accb52383a670d9cba1ee5cffe7726e620923d0
khaituonq <khaituong@gmail.com>
2022-11-19 10:38:35 +0700

    ul

--- backend/utils.js
+++ backend/utils.js
@@ -1,5 +1,5 @@
-import jwt from 'jsonwebtoken';
-import mg from 'mailgun-js';
+import jwt from "jsonwebtoken";
+import mg from "mailgun-js";
 
 export const generateToken = (user) => {
   return jwt.sign(
@@ -11,7 +11,7 @@ export const generateToken = (user) => {
     },
     process.env.JWT_SECRET,
     {
-      expiresIn: '30d',
+      expiresIn: "30d",
     }
   );
 };
@@ -22,14 +22,14 @@ export const isAuth = (req, res, next) => {
     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
-        res.status(401).send({ message: 'Invalid Token' });
+        res.status(401).send({ message: "Invalid Token" });
       } else {
         req.user = decode;
         next();
       }
     });
   } else {
-    res.status(401).send({ message: 'No Token' });
+    res.status(401).send({ message: "No Token" });
   }
 };
 
@@ -37,7 +37,7 @@ export const isAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401).send({ message: 'Invalid Admin Token' });
+    res.status(401).send({ message: "Invalid Admin Token" });
   }
 };
 
@@ -71,7 +71,7 @@ export const payOrderEmailTemplate = (order) => {
     </tr>
   `
     )
-    .join('\n')}
+    .join("\n")}
   </tbody>
   <tfoot>
   <tr>
